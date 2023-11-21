330a836554c24377b3f1e9e02716306233dd4fac
“Hector <hlopezbu@intercorp.com.pe>
2023-11-20 23:27:39 -0500

    feat: add API

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -8,6 +8,22 @@ app.http('message', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
+        let name2;
+
+        let request = new XMLHttpRequest();
+        request.open("GET", "http://jsonplaceholder.typicode.com/users");
+        request.send();
+        request.onload() = () => {
+          console.log(request);
+          if(request.status === 200) {
+            name2 = request.response;
+            console.log(JSON.parse(request.response));
+          } else {
+            name2 = request.status;
+            console.log(`error ${request.status} ${request.statusText}`)
+          }
+        }
+
         return { body: `Hello, ${name}!` };
     }
 });
